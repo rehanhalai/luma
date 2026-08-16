7e3cbf4b087a528add24a245e8dafc10bdd5a8c6
Rehan Halai <halairehan01@gmail.com>
2026-08-16 18:57:32 +0530

    stored player's position on server's map

--- apps/api/src/game/game.gateway.ts
+++ apps/api/src/game/game.gateway.ts
@@ -1,58 +1,67 @@
-import { ConnectedSocket, MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
-import {Server, Socket} from "socket.io"
+import {
+  ConnectedSocket,
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 
 export interface Player {
   id: number;
-  name : string;  
-  x:number;
-  y:number;
+  name: string;
+  x: number;
+  y: number;
 }
 @WebSocketGateway()
-export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect{
+export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server!: Server;
-  
-  private players = new Map<string,Player>()
+
+  private players = new Map<string, Player>();
   private readonly Max_Width = 800;
   private readonly Max_Height = 600;
 
   // on connection
- handleConnection(client: Socket, data : {name: string}) {
-  console.log(`player ${client.id} connected to socket server`)
+  handleConnection(client: Socket, data: { name: string }) {
+    console.log(`player ${client.id} connected to socket server`);
 
-  const newPlayer: Player = {
-    id: Number(client.id),
-    name : data.name,
-    x: Math.floor(Math.random() * (this.Max_Width -100 )),
-    y:Math.floor(Math.random() * (this.Max_Height -100 ))
+    const newPlayer: Player = {
+      id: Number(client.id),
+      name: data.name,
+      x: Math.floor(Math.random() * (this.Max_Width - 100)),
+      y: Math.floor(Math.random() * (this.Max_Height - 100)),
+    };
+
+    this.players.set(client.id, newPlayer);
+    client.emit('Players', Array.from(this.players.values()));
+    client.broadcast.emit('playerJoined', newPlayer);
   }
 
-  this.players.set(client.id,newPlayer)
-  client.emit("Players", Array.from(this.players.values()))
-  client.broadcast.emit("playerJoined",newPlayer)
- } 
-
   // on move
-  @SubscribeMessage("movement")
+  @SubscribeMessage('movement')
   handleMovement(
-    @ConnectedSocket() client : Socket,
-    @MessageBody() data :{x: number, y : number}
+    @ConnectedSocket() client: Socket,
+    @MessageBody() data: { x: number; y: number },
   ) {
-    if (this.players.get(client.id)){
-      const validX = Math.max(0,Math.min(this.Max_Width , data.x))
-      const validY = Math.max(0,Math.min(this.Max_Height , data.y))
-      
+    const player = this.players.get(client.id);
+    if (player) {
+      player.x = Math.max(0, Math.min(this.Max_Width, data.x));
+      player.y = Math.max(0, Math.min(this.Max_Height, data.y));
+
       client.broadcast.emit('playerMoved', {
         id: client.id,
-        x: validX,
-        y:validY
+        x: player.x,
+        y: player.y,
       });
     }
   }
 
   // on disconnection
   handleDisconnect(client: Socket) {
-    this.players.delete(client.id)
-    this.server.emit('playerLeft',client.id)
+    this.players.delete(client.id);
+    this.server.emit('playerLeft', client.id);
   }
 }
